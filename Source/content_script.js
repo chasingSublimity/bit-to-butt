35bca4c45bc159ab8d6cc4de6f72fbe97d15a98e
Blake Sager <blake.sager@activeprospect.com>
2017-12-08 17:47:17 -0600

    Convert to bit-to-butt

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,24 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
-	
+
 	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
 	    || node.classList.indexOf('ace_editor') > -1) {
 		return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,16 +32,13 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bBitCoin\b/g, "ButtCoin");
+	v = v.replace(/\bBitcoin\b/g, "Buttcoin");
+	v = v.replace(/\bbitcoin\b/g, "buttcoin");
+
 	textNode.nodeValue = v;
 }
-
-
